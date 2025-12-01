51e193e9c930942516c2724ab6f5e3b143b2db94
Leo Lacerda <51065340+leo-lacerda@users.noreply.github.com>
2025-12-01 09:58:46 +0000

    Adding new  folder

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Te amo, lindona! Se também me ama, dá aquele sorriso que eu tanto gosto!</h1>
+    return <h1>Te amo, lindona! Se também me ama, dá aquele sorriso que eu tanto gosto! 😘</h1>
 }
 
 export default Home;
